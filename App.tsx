e88c744105fdb1c860f6d9753b4183f8f9493be1
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-08-27 16:56:12 +0100

    created ui header

--- App.tsx
+++ App.tsx
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Text style={styles.expressions}>1+1</Text>
+        <View style={styles.resultContainer}>
+          <Text style={styles.equalOperator}>=</Text>
+          <Text style={styles.total}>2</Text>
+        </View>
+      </View>
+      
+      <View style={styles.main}>
+
+      </View>
+
+      <StatusBar style="light" translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ styles.ts
@@ -0,0 +1,37 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+const screenWidth = Dimensions.get('window').width;
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        width: screenWidth,
+        backgroundColor: '#2d2a37',
+        paddingVertical: 54,
+        paddingHorizontal: 32
+    },
+    expressions: {
+        color: '#6b6b6b',
+        fontSize: 23,
+        letterSpacing: 2,
+        textAlign: 'right'
+    },
+    resultContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center'
+    }, 
+    equalOperator: {
+        color: '#6b6b6b',
+        fontSize: 23
+    },
+    total: {
+        flex: 1,
+        color: '#fff',
+        fontSize: 36,
+        textAlign: 'right'
+    },
+    main: {
+
+    }
+});
