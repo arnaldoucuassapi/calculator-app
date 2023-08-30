ac558e2791d7084403c83dfe1fb27bf339a08b63
Arnaldo Ucuassapi <arnaldoucuassapi@gmail.com>
2023-08-30 02:47:01 +0100

    implementing some features

--- App.tsx
+++ App.tsx
@@ -1,49 +1,80 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { Alert, Text, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from './components/Button';
 import { useState } from 'react';
+import Calculos from './utils/calculos';
+
+enum Operations {
+  Sum = '+',
+  Sub = '-',
+  Multiplication = 'x',
+  Division = '÷'
+}
 
 export default function App() {
   const [number, setNumber] = useState<number>(0);
   const [expressions, setExpressions] = useState<string>("");
+  const [total, setTotal]= useState<number>(0) ;
 
-  function selectOperation(operation: string) {
+  const calculos = new Calculos();
 
-    const selectedOperation = number;
+  function calculate(operation: string) {
+
     setExpressions(state => {
-      return state ? `${state} ${operation} ${number} ` : `${number} ${operation}`;
+      return state ? `${state} ${number} ${operation}` : `${number} ${operation}`;
     });
 
     switch(operation) {
-      case '+':
+      case Operations.Sum:
+        setTotal(state => calculos.sum(state, number));
         break;
 
-      case '-':
+      case Operations.Sub:
+        setTotal(state => calculos.sub(state, number));
         break;
 
-      case '÷':
+      case Operations.Multiplication:
+        setTotal(state => calculos.multiplication(state, number));
         break;
 
-      case 'x':
+      case Operations.Division:
+        setTotal(state => calculos.division(state, number));
         break;
     }
+    
+    setNumber(total);
   }
 
   function addNumber(x: number) {
-    if (x != 0) {
-      
-    } else {
-      
+    if (number < 999999999999) {
+      if (number != 0) {
+        setNumber(state => Number(`${state}${x}`));
+      } else {
+        setNumber(x);
+      }
     }
   }
 
-  function clear() {
-
+  function addNumberWithComma() {
   }
 
-  function showCalc() {
+  function clearAll() {
+    setExpressions('');
+    setNumber(0);
+  }
 
+  function clearNumber() {
+    setNumber(0);
+  }
+
+  function deleteAddedLastNumber() {
+  }
+
+  function showCalculation() {
+    clearAll();
+    setNumber(total);
+    setTotal(0);
   }
 
   return (
@@ -52,44 +83,44 @@ export default function App() {
         <Text style={styles.expressions}>{expressions}</Text>
         <View style={styles.resultContainer}>
           <Text style={styles.equalOperator}>=</Text>
-          <Text style={styles.total}>2</Text>
+          <Text style={styles.total}>{number}</Text>
         </View>
       </View>
       
       <View style={styles.main}>
         <View style={styles.row}>
-          <Button title='CE' onPress={clear} type='function' />
-          <Button title='C' onPress={clear} type='function'/>
-          <Button title='%' onPress={() => selectOperation('%')} />
-          <Button title='÷' type='operation' onPress={() => selectOperation('÷')} />
+          <Button title='CE' onPress={clearAll} type='function' />
+          <Button title='C' onPress={clearNumber} type='function'/>
+          <Button title='Del' onPress={deleteAddedLastNumber} type='function' />
+          <Button title='÷' type='operation' onPress={() => calculate(Operations.Division)} />
         </View>
         
         <View style={styles.row}>
           <Button title='7' onPress={() => addNumber(7)} />
           <Button title='8' onPress={() => addNumber(8)} />
           <Button title='9' onPress={() => addNumber(9)} />
-          <Button title='x' type='operation' onPress={() => selectOperation('x')} />
+          <Button title='x' type='operation' onPress={() => calculate(Operations.Multiplication)} />
         </View>
         
         <View style={styles.row}>
           <Button title='4' onPress={() => addNumber(4)} />
           <Button title='5' onPress={() => addNumber(5)} />
           <Button title='6' onPress={() => addNumber(6)} />
-          <Button title='-' type='operation' onPress={() => selectOperation('-')} />
+          <Button title='-' type='operation' onPress={() => calculate(Operations.Sub)} />
         </View>
         
         <View style={styles.row}>
           <Button title='1' onPress={() => addNumber(1)} />
           <Button title='2' onPress={() => addNumber(2)} />
           <Button title='3' onPress={() => addNumber(3)} />
-          <Button title='+' type='operation' onPress={() => selectOperation('+')} />
+          <Button title='+' type='operation' onPress={() => calculate(Operations.Sum)} />
         </View>
         
         <View style={styles.row}>
           <Button title='|' />
           <Button title='0' onPress={() => addNumber(0)} />
-          <Button title=',' />
-          <Button title='=' type='calculator' onPress={showCalc} />
+          <Button title=',' onPress={addNumberWithComma} />
+          <Button title='=' type='calculator' onPress={showCalculation} />
         </View>
       </View>
 
